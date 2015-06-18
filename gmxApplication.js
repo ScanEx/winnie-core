4c0a2f967614a3f80d5e17879862010b9354a623
zverev <alexander.zverev@gmail.com>
2015-06-18 14:09:51 +0300

    ApplicationConstructor: use new sidebar; support sidebar options

--- gmxApplication.js
+++ gmxApplication.js
@@ -405,7 +405,8 @@ nsGmx.createGmxApplication = function(container, applicationConfig) {
         var layoutManager = cm.get('layoutManager');
         var widgetsContainer = layoutManager.getWidgetsContainer();
         if (config.sidebarWidget && L.Control.IconSidebarWidget) {
-            var sidebarWidget = new L.Control.IconSidebarWidget(widgetsContainer);
+            var sidebarWidget = new nsGmx.IconSidebarWidget(config.sidebarWidget);
+            sidebarWidget.appendTo(widgetsContainer);
             sidebarWidget.on('opening', function() {
                 var map = cm.get('map')
                 if (isPhone() && map) {
