e3c07b0ab5f598bcfcf03106dbf2ec4e627e7e41
zverev <alexander.zverev@gmail.com>
2016-03-02 16:41:25 +0300

    add debugWindow component

--- gmxApplication.js
+++ gmxApplication.js
@@ -1086,5 +1086,32 @@ nsGmx.createGmxApplication = function(container, applicationConfig) {
         return null;
     });
 
+    cm.define('debugWindow', ['map'], function() {
+        var map = cm.get('map');
+
+        var debugWindowControl = new(L.Control.extend({
+            onAdd: function(map) {
+                this._container = L.DomUtil.create('div', 'debugWindowControl');
+                this._controlCornerEl = L.DomUtil.create(
+                    'div',
+                    'leaflet-top leaflet-bottom leaflet-left leaflet-right debugWindowControl-controlCorner',
+                    map._controlContainer
+                );
+                map._controlCorners['debugwindow'] = this._controlCornerEl;
+                this.options.position = 'debugwindow';
+                return this._container;
+            },
+            log: function(str) {
+                L.DomUtil.addClass(this._container, 'debugWindowControl_active');
+                var el = L.DomUtil.create('div', '', this._container);
+                el.innerHTML = str;
+            }
+        }))();
+
+        debugWindowControl.addTo(map);
+
+        return debugWindowControl;
+    });
+
     return cm;
 };
