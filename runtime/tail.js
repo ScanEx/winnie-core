7c3d111b2dffc710a55d2e23ef7c853c968ed0f7
zverev <alexander.zverev@gmail.com>
2016-06-06 18:06:07 +0300

    add debug key to config / debug layers tree

--- runtime/tail.js
+++ runtime/tail.js
@@ -1,14 +1,32 @@
-cm.define('globals', ['layersTree', 'layersHash', 'calendar', 'rawTree', 'config', 'map'], function() {
-    if (!cm.get('config').app.globals) {
+// <Boolean> debug.globals
+// <Boolean> debug.layersTree
+cm.define('debug', ['layersTree', 'layersHash', 'calendar', 'rawTree', 'config', 'map'], function() {
+    var debugCfg = cm.get('config').debug;
+    if (!debugCfg) {
         return null;
     }
 
-    window.cal = cm.get('calendar');
-    window.lt = cm.get('layersTree');
-    window.lh = cm.get('layersHash');
-    window.rt = cm.get('rawTree');
-    window.cfg = cm.get('config');
-    window.map = cm.get('map');
+    debugCfg = L.extend({
+        globals: true,
+        layersTree: true
+    });
+
+    if (debugCfg.globals) {
+        window.cal = cm.get('calendar');
+        window.lt = cm.get('layersTree');
+        window.lh = cm.get('layersHash');
+        window.rt = cm.get('rawTree');
+        window.cfg = cm.get('config');
+        window.map = cm.get('map');
+    }
+
+    if (debugCfg.layersTree) {
+        var layersTree = cm.get('layersTree');
+
+        layersTree.on('childChange', function (node) {
+            console.log(node.get('visible'), node.get('id'), node.get('properties').title);
+        });
+    }
 
     return null;
 });
@@ -26,17 +44,17 @@ cm.define('debugWindow', ['logger', 'config', 'map'], function() {
 
     var debugWindowControl = new(L.Control.extend({
         // options.logger
-        initialize: function (options) {
+        initialize: function(options) {
             this.loggerCollection = options.logger;
             this.loggerCollection.on('add', this.render, this);
             this.render();
         },
-        render: function () {
+        render: function() {
             if (!this._container || !this.loggerCollection.length) {
                 return;
             }
             L.DomUtil.addClass(this._container, 'debugWindowControl_active');
-            this.loggerCollection.map(function (msgModel) {
+            this.loggerCollection.map(function(msgModel) {
                 var el = L.DomUtil.create('div', '', this._container);
                 el.innerHTML = msgModel.get('message');
             }.bind(this));
