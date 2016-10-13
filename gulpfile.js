d1a75dbfb602d51a4c53c773a686e231619b41b1
zverev <alexander.zverev@gmail.com>
2016-10-13 22:43:48 +0300

    watch css

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,32 @@ var concat = require('gulp-concat')
 var iife = require('gulp-iife')
 var gulp = require('gulp')
 
+var jsSrc = [
+    'lib/PagingView.js',
+    'lib/FullscreenControlMixin.js',
+    'lib/FullscreenPagingPaneControl.js',
+    'lib/MobileButtonsPaneControl.js',
+    'lib/gmxTreeParser.js',
+    'runtime/_header.js',
+    'runtime/core.js',
+    'runtime/map.js',
+    'runtime/layers.js',
+    'runtime/controls.js',
+    'runtime/containers.js',
+    'runtime/widgets.js',
+    'runtime/tail.js',
+    'runtime/_footer.js'
+]
+
+var cssSrc = ['gmxApplication.css']
+
 gulp.task('css', function() {
-    return gulp.src('gmxApplication.css')
+    return gulp.src(cssSrc)
         .pipe(gulp.dest('dist'))
 })
 
 gulp.task('js', function() {
-    return gulp.src([
-            'lib/PagingView.js',
-            'lib/FullscreenControlMixin.js',
-            'lib/FullscreenPagingPaneControl.js',
-            'lib/MobileButtonsPaneControl.js',
-            'lib/gmxTreeParser.js',
-            'runtime/_header.js',
-            'runtime/core.js',
-            'runtime/map.js',
-            'runtime/layers.js',
-            'runtime/controls.js',
-            'runtime/containers.js',
-            'runtime/widgets.js',
-            'runtime/tail.js',
-            'runtime/_footer.js'
-        ])
+    return gulp.src(jsSrc)
         .pipe(concat('gmxApplication.js'))
         .pipe(iife())
         .pipe(gulp.dest('dist'))
@@ -32,5 +36,5 @@ gulp.task('js', function() {
 gulp.task('default', ['js', 'css'])
 
 gulp.task('watch', ['default'], function () {
-    gulp.watch(['lib/*', 'runtime/*'], ['default'])
+    gulp.watch([].concat(jsSrc, cssSrc), ['default'])
 })
